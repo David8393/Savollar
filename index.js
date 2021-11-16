f9d94a7715153e2cfee44bc2fbf7d01de5ac1b39
David8393 <jo974586david@gmail.com>
2021-11-16 23:25:16 +0500

    savollar

---
+++ index.js
@@ -0,0 +1,197 @@
+// Savollar:
+// 1.String
+// 1.  var a = 'Web\"brain\" Academy'
+// console.log(a);
+// Ushbu holat to’g’rimi? Va agar to’g’ri bo’lsa consoleda qanday javob chiqadi?
+// 2. var a = 'Web "Brain" Academy';
+
+// var b = 'Apple';
+
+// var c = `Web "Brain" \n Academy \t ${b}`;
+
+// console.log(c);
+// Consoleda qanday javob chiqadi?
+// 3.var b = 'Apple'
+// var c = `Web "Brain" Academy`;
+
+// console.log(typeof (c + b));
+// Consoleda qanday javob chiqadi?
+// 4. var a = 'Apple';
+// a [0] = 'B'
+
+// console.log(a[0]);
+// Consoleda qanday javob chiqadi?
+// 5. var a = "Apple Banana"
+
+// console.log(a.indexOf('pl', 3));
+// Consoleda qanday javob chiqadi?
+// 2.Number
+// 6. var a = 1;
+
+// var b = '10';
+
+// console.log(b - a + b);
+//  Consoleda qanday javob chiqadi?
+// 7. var a = 1;
+// var b = '10';
+
+// var c = 'A';
+
+// console.log (a - b + b + c)
+// console.log (a - b + b - c);
+// 8. let x = '100';
+
+// let y = '10';
+
+// let z = x / y;
+
+// console.log (z - y);
+// Consoleda qanday javob chiqadi?
+// 9. let x = 99.456789;
+
+// console.log(x.toFixed(0));
+// Consoleda qanday javob chiqadi?
+// 10. let x = 99.456789;
+
+// console.log (x.toFixed(0));
+// console.log (Math.ceil (x));
+// console.log (Math.floor(x));
+// Consoleda qanday javob chiqadi?
+// 3.Array
+// 11. var arr = ['Webbrain academy', 'Wbba', 100];
+
+// arr.push(0);
+
+// arr.shift(1)
+
+// arr.unshift(0)
+
+// console.log(arr);
+// Consoleda qanday javob chiqadi?
+// 12. Join methodi qanday vazifani bajaradi?
+
+// 13. var arr = ['Webbrain Academy', 100, 01, 'Wbba']
+
+// console.log(arr.join('0'));
+//  Consoleda qanday javob chiqadi?
+// 14. var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// console.log(arr.slice(4, 9));
+// console.log(arr);
+// console.log(arr.splice(4, 9));
+// console.log(arr);
+// Consoleda qanday javob chiqadi?
+
+// 15. var arr = [0, 1, 2, 3,[4, 5, 6, 7], 8, 9, 10]
+// Ushbu holatda array ichidagi arrayga qanday qilib kiriladi?
+// 4.Object
+// 16. const object = {
+//     name: 'David',
+//     surname: 'Croft',
+//     status: [
+//         { level: 'Bachelor', finished: 'High School' futurePlan: 'Developer'}
+//     ]
+// }
+// console.log(object);
+// Belgilab ko’rsatilgan joydagi ‘Developer’ so’zini consoleda qanday qilib chiqaramiz?
+// 17. const object = {
+//     name: 'David'
+// }
+// console.log(object);
+// Berilayotgan Object ga qanday qilib qo’shimcha ma’lumot kiritishimiz mumkin?
+// Ikki xil usulini yozing!
+// 18. const object = {
+//     name: 'David'
+// }
+// object['name'] = 'Otabek'
+// const key = [surname]
+// console.log(object);
+// Consoleda qanday javob chiqadi?
+// 19. Biz Object.keys va Object.values larni qachon, qanday holatlarda ishlatamiz?
+
+// 20. const user1 = {
+//     name: 'David',
+//     surname: 'Croft'
+// }
+// const user2 = {
+//   name: 'David',
+//   surname: 'Croft',
+// };
+
+// const { name, surname } = user1;
+// const { name, surname } = user2;
+// console.log(name);
+// Ushbu yo’l to’g’rimi? Console da qanday javob chiqadi?
+// 5.Function
+// 21. function a(name, surname, age) {
+//     console.log('My name ' + name);
+//     console.log('My surname ' + surname);
+//     console.log('My age ' + age);
+// }
+
+// a(19, 'Juraev', 'Otabek')
+// Consoleda qanday javob chiqadi?
+
+// 22. function a(name) {
+//     console.log('My name ' + name);
+    
+// }
+// console.log(a('Otabek'));
+// Consoleda qanday javob chiqadi?
+
+// 23. function a() {
+//     var name = 'Otabek';
+//     return name + ' Otabek'
+// }
+// console.log(a('Otabek'));
+// Consoleda qanday javob chiqadi?
+
+// 24. function welcome(name) {
+//   console.log('Hello ' + name + ' welcome WBBA');
+//   return name + 'Otabek';
+// }
+// name = 'David ';
+// console.log(welcome(name));
+// Consoleda qanday javob chiqadi?
+
+// 25. function a(number1, number2) {
+//   return number1 + number2;
+// }
+// console.log(a(number1, number2));
+// Consoleda qanday javob chiqadi?
+
+// 6.if else, Switch
+
+
+// 26. function sum(a, b) {
+//     if (a > b) {
+//       console.log(a-b);
+//     }
+//     else if (b > a) {
+//         console.log(b-a);
+//     }
+//     else {
+//         console.log('Problem');
+//     }
+// }
+// sum(1.25, 1.25)
+// Consoleda qanday javob chiqadi?
+
+// 27. function test() {
+//     var num = 4;
+//     switch (num) {
+//       case 1:
+//         console.log('num');
+//         break;
+//       case 2:
+//         console.log('num');
+//         break;
+//       case 3:
+//         console.log('num');
+//         break;
+//     }
+// }
+// test()
+// Xatolik bor. Ushbu holatni to’g’rilash uchun nima qilshimiz kerak?
+
+
